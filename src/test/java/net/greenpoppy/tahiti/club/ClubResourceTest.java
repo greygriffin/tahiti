684ec315b6abf95d922c4279be716d9d868e5ee1
David Price <david.price@codento.com>
2018-04-24 16:40:13 +0300

    Remove unnecessary imports

--- src/test/java/net/greenpoppy/tahiti/club/ClubResourceTest.java
+++ src/test/java/net/greenpoppy/tahiti/club/ClubResourceTest.java
@@ -1,6 +1,5 @@
 package net.greenpoppy.tahiti.club;
 
-import net.greenpoppy.tahiti.member.MemberEntity;
 import net.greenpoppy.tahiti.resource.ResourceTestBase;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,21 +8,16 @@ import static org.junit.Assert.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
-import java.nio.charset.Charset;
-import java.security.Principal;
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
